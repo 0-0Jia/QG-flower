f6e2ee691d7eb697d4dfea0ecba16c555770f5a2
0-0Jia <1036748415@qq.com>
2020-04-23 20:06:54 +0800

    4.23大致完成ui

--- src/pages/about/search/search.vue
+++ src/pages/about/search/search.vue
@@ -1,37 +1,171 @@
 <style scoped>
 .search {
-  width: 100%;
+  position: relative;
+  font-family: 微软雅黑;
 }
-.title {
+
+/*图标*/
+.searchIcon {
+  position: absolute;
+  top: 10px;
+  right: 13px;
+  width: 16px;
+  height: 16px;
+}
+.delete-icon {
+  position: absolute;
+  top: 1px;
+  right: 1%;
+  width: 30px;
+  height: 30px;
+}
+.icon {
+  position: absolute;
+  top: 2px;
+  left: 105px;
+  width: 30px;
+  height: 30px;
+}
+
+/*上方搜索*/
+.container-one {
+  position: relative;
   width: 90%;
-  font-size: 25px;
-  font-weight: 500;
-  margin: 5% auto;
-  text-align: left;
+  margin: 20px auto;
+  margin-bottom: 24px;
+}
+
+.search-container {
+  position: relative;
+  width: 78%;
+  height: 36px;
+  margin-left: 4%;
+  background-color: #edf0f4;
+  border-radius: 20px;
+}
+
+.search-input {
+  width: 80%;
+  height: 26px;
+  padding: 5px 15px;
+}
+
+.cancel {
+  position: absolute;
+  top: 5px;
+  right: 1%;
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+/*下方常用选项*/
+.container-two {
+  width: 90%;
+  margin: 10px auto;
+}
+.select-title {
+  position: relative;
+  margin: 10px 0;
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+.search-all-item {
+  margin: 0 auto;
+}
+.search-item {
+  display: inline-block;
+  margin: 4px 6px;
+  padding: 5px 16px;
+  border-radius: 15px;
+  background-color: #f5f5f5;
+  font-size: 15px;
+  font-weight: 600;
+  color: #00000080;
 }
 </style>
 
 <template>
     <div class="search">
-    <h1 class="title">搜索页面</h1>
-    <div></div>
-  </div>
+        <div class="container-one">
+            <div class="search-container">
+                <input class="search-input">
+                <img src="../../../../static/images/search.png" alt="搜索" class="searchIcon">
+            </div>
+            <div class="cancel" @click="toAbout">取消</div>
+        </div>
+        <div class="container-two">
+            <div class="select-flower">
+                <div class="select-title">
+                    <h1>热门花卉</h1>
+                    <img src="../../../../static/images/hot.png" class="icon">
+                </div>
+                <div class="search-all-item">
+                    <div
+                        class="search-item"
+                        v-for="(flower, index) in hotList"
+                        :key="index"
+                    >{{flower.item}}</div>
+                </div>
+            </div>
+            <div class="select-flower">
+                <div class="select-title">
+                    <h1>搜索历史</h1>
+                    <img src="../../../../static/images/history.png" class="icon">
+                    <img src="../../../../static/images/delete.png" class="delete-icon">
+                </div>
+                <div class="search-all-item">
+                    <div
+                        class="search-item"
+                        v-for="(flower, index) in historyList"
+                        :key="index"
+                    >{{flower.item}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-
 export default {
-  data () {
+  data() {
     return {
-     
-    }
+      hotList: [
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "玫瑰" }
+      ],
+      historyList: [
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "茉莉花" },
+        { item: "玫瑰" },
+        { item: "玫瑰" }
+      ]
+    };
   },
-
 
   methods: {
- 
-  },
-
-}
-
+    toAbout() {
+      mpvue.navigateBack({
+          delta: 1
+      });
+    // wx.switchTab({
+    //     url: "../../../pages/about/main"
+    // })
+    }
+  }
+};
 </script>
